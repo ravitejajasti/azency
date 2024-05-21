901f78aa7b8a76e13985278a1fc0dde4be24edc2
WinRT <jasti.mba@gmail.com>
2024-05-20 19:07:56 -0500

    ajax task create/update with delete api

--- projects/urls.py
+++ projects/urls.py
@@ -16,14 +16,31 @@ urlpatterns = [
     path('<int:project_id>/tasks/<int:pk>/edit/', TaskUpdateView.as_view(), name='task_edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#### API URLs
 # from django.urls import path
-# from .views import ProjectListAPIView, TaskListAPIView
+# from .views import (
+#     ProjectListCreateAPIView,
+#     ProjectRetrieveUpdateAPIView,
+#     TaskListCreateAPIView,
+#     TaskRetrieveUpdateDestroyAPIView,
+#     SectionTaskListAPIView,
+#     CommentListCreateAPIView,
+# )
 
-# urlpatterns += [
-#     path('api/projects/', ProjectListAPIView.as_view(), name='api_project_list'),
-#     path('api/tasks/<int:project_id>/', TaskListAPIView.as_view(), name='api_task_list'),
-# ]
+# urlpatterns = [
+#     path('', ProjectListCreateAPIView.as_view(), name='project_list'),
+#     path('new/', ProjectListCreateAPIView.as_view(), name='project_create'),
+#     path('<int:pk>/', ProjectRetrieveUpdateAPIView.as_view(), name='project_detail'),
+#     path('<int:pk>/edit/', ProjectRetrieveUpdateAPIView.as_view(), name='project_edit'),
+#     path('<int:project_id>/tasks/', TaskListCreateAPIView.as_view(), name='task_list'),
+#     path('<int:project_id>/tasks/new', TaskListCreateAPIView.as_view(), name='task_create'),
+#     path('<int:project_id>/tasks/<int:pk>/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task_detail'),
+#     path('tasks/<int:pk>/edit/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task_edit'),
+#     path('<int:project_id>/sections/tasks/', SectionTaskListAPIView.as_view(), name='section_task_list'),
+#     path('<int:project_id>/tasks/<int:pk>/comments/', CommentListCreateAPIView.as_view(), name='comment_list_create'),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#### API URLs
+
 from django.urls import path
 from .views import TaskDeleteAPIView
 
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from .models import Project, Task, Section
+from .models import Project, Task, Section, Comment
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, FormMixin
 from django.urls import reverse_lazy
@@ -236,21 +236,90 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
 
 
 ###### API related Classes
-# from rest_framework.generics import ListAPIView
-# from .serializers import ProjectSerializer, TaskSerializer
 
-# class ProjectListAPIView(ListAPIView):
+
+# from rest_framework import generics, status
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.response import Response
+# from .models import Project, Task, Section
+# from .serializers import ProjectSerializer, TaskSerializer, CommentSerializer
+# from django.contrib.auth.models import User
+# from django.shortcuts import get_object_or_404
+
+# class ProjectListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Project.objects.annotate(total_tasks=Count('tasks'))
+#     serializer_class = ProjectSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def perform_create(self, serializer):
+#         serializer.save(created_by=self.request.user)
+
+# class ProjectRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
 #     queryset = Project.objects.all()
 #     serializer_class = ProjectSerializer
+#     permission_classes = [IsAuthenticated]
 
-# class TaskListAPIView(ListAPIView):
-#     queryset = Task.objects.all().select_related('project').prefetch_related('comments')  # Improve performance
-
+# class TaskListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Task.objects.all()
 #     serializer_class = TaskSerializer
+#     permission_classes = [IsAuthenticated]
 
 #     def get_queryset(self):
-#         project_id = self.kwargs['project_id']
-#         return Task.objects.filter(project_id=project_id)
+#         return Task.objects.filter(project_id=self.kwargs['project_id'])
+
+#     def perform_create(self, serializer):
+#         project = get_object_or_404(Project, id=self.kwargs['project_id'])
+#         serializer.save(project=project)
+
+# class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#     permission_classes = [IsAuthenticated]
+
+# class SectionTaskListAPIView(generics.ListAPIView):
+#     serializer_class = TaskSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         return Task.objects.filter(section__project_id=self.kwargs['project_id'])
+
+# class TaskDetailView(generics.RetrieveUpdateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance, data=request.data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+
+#         if 'X-Requested-With' in request.headers:
+#             return Response({
+#                 'success': True,
+#                 'redirect_url': self.get_success_url(instance),
+#                 'message': 'Task updated successfully.',
+#                 'pk': instance.pk,
+#             })
+
+#         return Response(serializer.data)
+
+#     def get_success_url(self, instance):
+#         return reverse_lazy('projects:task_detail', kwargs={'project_id': instance.project.id, 'pk': instance.pk})
+
+# class CommentListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         return Comment.objects.filter(task_id=self.kwargs['pk'])
+
+#     def perform_create(self, serializer):
+#         task = get_object_or_404(Task, id=self.kwargs['pk'])
+#         serializer.save(task=task)
+
 
 from rest_framework.generics import DestroyAPIView
 from .models import Task
